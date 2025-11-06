bd5afbe8768dfd09a86f2d5264a9c322a9218185
CupChunGae <c_nim@hotmail.com>
2025-11-06 00:42:59 +0000

    Added a complete report and filepath arguments

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import sys
+from stats import num_of_words, character_count, sorting_dict
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return(file_contents)
 
 def main():
-        text = get_book_text("./books/frankenstein.txt")
+        text = get_book_text(file_direct)
         print(text)
         
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    print(sys.argv)
+file_direct = sys.argv[1]
+big_dict = sorting_dict(character_count(get_book_text(file_direct)))
 
-from stats import num_of_words
-num_of_words(get_book_text("./books/frankenstein.txt"))
+print(f"""
+    ======= BOOKBOT =======
+    Analyzing book found at {file_direct} . . .
+    ------- Word Count -------
+    {num_of_words(get_book_text(file_direct))}
+    ------- Character Count -------
+""")
+for i in range(len(big_dict)):
+    print(f"{big_dict[i]['char']}: {big_dict[i]['num']}")
+    
+print("======= END =======")
--- stats.py
+++ stats.py
@@ -2,3 +2,22 @@ def num_of_words(text):
     words_to_count = text.split()
     print(f"Found {len(words_to_count)} total words")
     
+def character_count(text):
+    from collections import Counter
+    returning_dict = {}
+    for character in text.lower():
+        if character not in returning_dict:
+            returning_dict[character] = 1
+        else:
+            returning_dict[character] += 1
+    return returning_dict
+
+def sorting_dict(text_dict):
+    #take in the dictionary and turn it into a list of dictionaries
+    def sort_on(list_of_dicts):
+        return list_of_dicts["num"]
+    returning_arr = []
+    for item in text_dict:
+        returning_arr.append({"char":item, "num": text_dict[item]})
+    returning_arr.sort(reverse=True, key = sort_on)
+    return returning_arr
